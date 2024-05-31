7e4b10367a81f0b72b58b5d13de7fa39bf97d174
MoriKen <a.ladnak25@gmail.com>
2024-05-31 11:55:34 +0200

    models + stuff

---
+++ soc/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . models import *
+
+admin.site.register(Tema)
+admin.site.register(Ucitel)
+admin.site.register(Student)
+admin.site.register(Odbor)
+# Register your models here.
